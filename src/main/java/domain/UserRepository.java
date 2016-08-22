0132a5fed9838437b036e4559937344488e2590e
Naveen Abbigeri <naveen.abbigeri@accionlabs.com>
2016-08-22 11:23:25 +0530

    Initial commit

---
+++ src/main/java/domain/UserRepository.java
@@ -0,0 +1,12 @@
+package domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByUserName(String username);
+    
+}
